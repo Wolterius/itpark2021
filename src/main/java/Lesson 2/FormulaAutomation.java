7fdc0f777b82d0112c7ff36241cd08a1dc71628c
Andrey <93442947+Wolterius@users.noreply.github.com>
2021-11-06 11:41:23 +0300

    Update FormulaAutomation.java

--- src/main/java/Lesson 2/FormulaAutomation.java
+++ src/main/java/Lesson 2/FormulaAutomation.java
@@ -1,21 +1,19 @@
 public class FormulaAutomation {
-    /*Формула для рассчета средней кинетической энергии молекул E = (3/2)*K*T,
-    где k - Постоянная Больцмана равная 1.38 * 10 в -23 степени
-    T - температура газа
-    */
-
+    
     public static void main(String[] args) {
 
-        System.out.println(keenetikmolecul(150));
-        System.out.println(keenetikmolecul(600));
-        System.out.println(keenetikmolecul(1400));
+        System.out.println(keenetikMolecul(150));
+        System.out.println(keenetikMolecul(600));
+        System.out.println(keenetikMolecul(1400));
     }
-
-    public static double keenetikmolecul(double T) {
-        double k = 1.38 * 0.000000000000000000000001; //Постоянная Больцмана
+    
+    public static double keenetikMolecul(double T) {
+        double k = 1.38 * 0.000000000000000000000001; 
+    /*Формула для рассчета средней кинетической энергии молекул E = (3/2)*K*T,
+    где k - Постоянная Больцмана равная 1.38 * 10 в -23 степени
+    T - температура газа */
         double e = (3/2) * k * T;
         return e;
-
     }
 }
 
