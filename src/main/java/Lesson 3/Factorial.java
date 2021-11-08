c7411aff78204eff08a1534683500da0f04a5fd2
Andrey <93442947+Wolterius@users.noreply.github.com>
2021-11-08 16:23:31 +0300

    Update Factorial.java

--- src/main/java/Lesson 3/Factorial.java
+++ src/main/java/Lesson 3/Factorial.java
@@ -1,13 +1,13 @@
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println(FactorialResult(5));
+        System.out.println(factorialResult(5));
     }
 
-    public static int FactorialResult(int a) {
+    public static int factorialResult(int a) {
         if (a <= 1) {
             return 1;
         } else {
-            return a * FactorialResult(a - 1);
+            return a * factorialResult(a - 1);
         }
     }
 }
