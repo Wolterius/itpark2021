b4b57f53677ed55951b8e505563a5b9442314516
wolterius <wolterius@gmail.com>
2021-11-08 10:44:00 +0300

    We implement the factorial

---
+++ src/main/java/Lesson 3/Factorial.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(FactorialResult(5));
+    }
+
+    public static int FactorialResult(int f) {
+        if (f <= 1) {
+            return 1;
+        } else {
+            return f * FactorialResult(f - 1);
+        }
+    }
+}
