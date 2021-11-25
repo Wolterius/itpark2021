8f72be9fc12a1a8bb310e7d1a279372ef154da0f
wolterius <wolterius@gmail.com>
2021-11-25 12:52:07 +0300

    Search min, max and average meaning

--- src/main/java/Lesson6/HomeWork1.java
+++ src/main/java/Lesson6/HomeWork1.java
@@ -1,29 +1,50 @@
 package Lesson6;
-import java.util.Arrays;
+
+import java.util.Scanner;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-        int n =10;
-        double [] array = new double[n];
-
-        for(int i = 0; i < array.length; i++) {
-            array[i] = Math.random() * 11;
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int n = console.nextInt();
+        double[] array = new double[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Math.random();
+            System.out.println(array[i]);
         }
-        System.out.println(Arrays.toString(array));
+        System.out.println("Максимальное число в массиве: " + maxArray(array));
+        System.out.println("Минимальное число в массиве: " + minArray(array));
+        System.out.println("Среднее число в массиве: " + avgArray(array));
+    }
 
+    public static double maxArray(double[] array) {
         double max = array[0];
-        double min = array[0];
-        double avg = 0;
-
-        for(int o = 0; o < array.length; o++) {
-            if(max < array[o])
-                max = array[o];
-            if(min > array[0])
-                min = array[0];
-            avg = avg + array[o] / array.length;
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
         }
-        System.out.println(max);
-        System.out.println(min);
-        System.out.println(avg);
+        return max;
+    }
+
+    public static double minArray(double[] array) {
+        double min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static double avgArray(double[] array) {
+        double avg = 0;
+        int i = 0;
+        while (i < array.length) {
+            avg += array[i];
+            i++;
+        }
+        avg = avg / array.length;
+        return avg;
     }
 }
